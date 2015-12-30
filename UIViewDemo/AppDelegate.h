b478d812931bb3cb19fdf8208339f54022fce0d1
Dang Vu Duy <Dev@dangs-macbook-air.local>
2015-12-30 09:40:23 +0700

    Initial Commit

---
+++ UIViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIViewDemo
+//
+//  Created by Dang Vu Duy on 12/30/15.
+//  Copyright © 2015 Dang Vu Duy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIViewDemo
+//
+//  Created by Dang Vu Duy on 12/30/15.
+//  Copyright © 2015 Dang Vu Duy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
